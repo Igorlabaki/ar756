84adab5dfb34930ff85e5ca96a1144bce07da2e4
Igor Goncalo <igorlabakig@gmail.com>
2023-05-12 17:47:50 +0100

    da

--- src/hook/useModalsContext.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-
-import {ModalsContext} from '../context/ModalsContext'
-
-const useModalsContext = () => useContext(ModalsContext)
-
-export default useModalsContext
