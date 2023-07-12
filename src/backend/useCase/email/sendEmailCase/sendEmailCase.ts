c906c657a3b85897ed60f04aff08c02ab11e1b25
Igor Labaki Gonçalo <igorlabakig@gmail.com>
2023-07-12 16:30:46 +0100

    Update sendEmailCase.ts

--- src/backend/useCase/email/sendEmailCase/sendEmailCase.ts
+++ src/backend/useCase/email/sendEmailCase/sendEmailCase.ts
@@ -33,7 +33,7 @@ class SendEmailCase {
                                 <h1 style="color: #333; width: 100%; text-align: center; margin-top: 10px;">Olá ${nome}, recebemos a sua mensagem!</h1>
                                 <p style="font-size: 18px; width: 60%; margin: 10px auto; text-align: center;">Agradecemos o seu interesse em conhecer a AR756. Simulamos um orçamento para seu evento, por gentileza clique no botão abaixo para ver a proposta.</p>
                                 <div style="width: 100%; text-align: center;margin-top: 40px;">
-                                <a href="http://localhost:3000/orcamento/byId/${orcamentoId}" style="text-decoration: none;">
+                                <a href="https://ar756.vercel.app/orcamento/byId/${orcamentoId}" style="text-decoration: none;">
                                     <button style="background-color: black; color: white; font-size: 20px; padding: 10px 20px; border-radius: 5px; display: inline-block;">Orçamento</button>
                                 </a>
                                 </div>
