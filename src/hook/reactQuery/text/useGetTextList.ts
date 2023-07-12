65a7f95e04bf4640cd9f3b2cea20d59aee186987
Igor Goncalo <igorlabakig@gmail.com>
2023-07-12 14:42:52 +0100

    gfdg

--- src/hook/reactQuery/text/useGetTextList.ts
+++ src/hook/reactQuery/text/useGetTextList.ts
@@ -1,19 +1,21 @@
 import { api } from "@/service/axios";
 import { useQuery } from "@tanstack/react-query";
 
-export default function useGetTextList(){
-
+export default function useGetTextList() {
     const {
         data: textList,
         isError: errortextList,
-        isLoading: textListIsLoading, 
-        } = useQuery({
-            queryKey: ["textList"],
-            queryFn: async () => {
-            return  api.get(`/api/text/list`
-            ).then((resp ) => resp.data)
+        isLoading: textListIsLoading,
+    } = useQuery({
+        queryKey: ["textList"],
+        queryFn: async () => {
+        return api.get(`/api/text/list`, {
+            headers: {
+            'Access-Control-Allow-Origin': 'https://ar756.vercel.app',
             },
-        }
-    );
-    return {textList, errortextList, textListIsLoading}
+        }).then((resp) => resp.data);
+        },
+    });
+
+    return { textList, errortextList, textListIsLoading };
 }
